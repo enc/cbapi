c2daee10e4ceda22168e357812b5f5321991d799
Jonatan Reiners <jr@encc.de>
2014-02-08 22:21:47 +0100

    get and define simple properties

--- lib/cbapi/base.rb
+++ lib/cbapi/base.rb
@@ -1,21 +1,12 @@
 module Cbapi
   class Base
 
-    def api_key(key)
-      @key = key
-    end
-
     def cb_url(url_string)
       @url = url_string
     end
 
     def get_url(*parameters)
-      result = "http://api.crunchbase.com/v/1/" + fill_in(parameters)
-      if @key
-        return result + "?api_key=" + @key
-      else
-        return result
-      end
+      "http://api.crunchbase.com/v/1/" + fill_in(parameters)
     end
     def fill_in(parameters)
       result = @url.clone
@@ -23,6 +14,23 @@ module Cbapi
         result["<>"] = param
       end
       return result
+    end
+    def define_property(name)
+      (class << self; self; end).class_eval do
+        define_method(name) do
+          if @entity
+            @entity[name.to_s]
+          else
+            nil
+          end
+        end
+      end
+    end
+    def get(*something)
+      @entity = API.retrieve(get_url(*something))
+    end
+    def entity
+      @entity
     end
   end
 end
--- spec/cbapi/base_spec.rb
+++ spec/cbapi/base_spec.rb
@@ -13,15 +13,27 @@ module Cbapi
       expect(subject.get_url("fritz")).to eq("http://api.crunchbase.com/v/1/product/fritz.js")
     end
 
-    it 'creates url with api' do
-      subject.cb_url("product/<>.js")
-      subject.api_key  "vtbn6ju62cp2apg9mjmuva86"
-      expect(subject.get_url("john")).to eq("http://api.crunchbase.com/v/1/product/john.js?api_key=vtbn6ju62cp2apg9mjmuva86")
-    end
-
     it 'can handle more paramter'do
       subject.cb_url("<>/<>.js")
       expect(subject.get_url("product","john")).to eq("http://api.crunchbase.com/v/1/product/john.js")
     end
+
+    context "get content" do
+      let(:company) { File.open(File.join(File.dirname(__FILE__), "..","json","company.js")).read }
+      it "defines it property" do
+
+        subject.define_property :name
+        expect(subject).to respond_to(:name)
+      end
+      it "get data" do
+        API.should_receive(:retrieve).at_least(:once).with("http://api.crunchbase.com/v/1/company/facebook.js").and_return(JSON.parse(company))
+        subject.cb_url("company/<>.js")
+        subject.define_property :name
+        subject.get("facebook")
+        expect(subject.name).to eq("Facebook")
+      end
+
+    end
+
   end
 end
